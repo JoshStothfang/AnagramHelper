071425736d46f2818bf43961759c20af57e58656
Josh Stothfang <josh@joshstothfang.com>
2023-12-19 17:19:41 -0500

    Added Counter

--- AnagramHelper.py
+++ AnagramHelper.py
@@ -1,6 +1,14 @@
 import sys
+from collections import Counter
 
 input = sys.argv[1]
 
 #Remove spaces and capitalize all letters
 input = input.replace(" ", "").upper()
+
+#Create list of characters
+inputChars = [char for char in input]
+
+#Create Counter of characters
+counter = Counter(inputChars)
+
